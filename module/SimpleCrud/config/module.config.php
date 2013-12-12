e39b0a8b29e0f7cf69e84cc678993cc5f575fee6
Andy Librian <andyajadeh@gmail.com>
2013-12-12 13:35:00 +0700

    add route

--- module/SimpleCrud/config/module.config.php
+++ module/SimpleCrud/config/module.config.php
@@ -1,4 +1,33 @@
 <?php
 
 return array(
+    'router' => array(
+        'routes' => array(
+            'simple-crud' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/crud',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'SimpleCrud\Controller',
+                        'controller' => 'Contact',
+                        'action' => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'SimpleCrud\Controller\Contact' => 'SimpleCrud\Controller\ContactController',
+        ),
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+            'simple-crud/contact/index' => __DIR__ . '/../view/simple-crud/contact/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 );
---
+++ module/SimpleCrud/src/SimpleCrud/Controller/ContactController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SimpleCrud\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ContactController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+}
