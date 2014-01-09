cddd89ad99ce466d81f01303f8d06d7ed5ade84e
Andy Librian <andyajadeh@gmail.com>
2014-01-09 08:02:49 +0700

    add twitter bootstrap bundle

--- config/application.config.php
+++ config/application.config.php
@@ -7,6 +7,7 @@ return array(
         'DoctrineModule',
         'DoctrineORMModule',
         'ZendDeveloperTools',
+        'TwbBundle',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
